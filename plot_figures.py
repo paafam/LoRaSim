5d8e1972f59c9e236e0dda385d21751ac114f5fc
PaFam <pafam@esp.sn>
2020-07-13 01:00:19 +0000

    ajouté script d'automatisation des simulations

---
+++ plot_figures.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+This python code allows us to plot some figures from the generated simulation results file
+
+@author: Pafam
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# results filename to load
+result_filepath = './sim_results_exp1.dat'
+# we perform 100 simulation realization for each number of nodes from 100 to 1600
+# the number of realization should be updated according to the simulation results in the result file
+nrRealization = 100
+
+# Read data from sim_results_file
+simtime, avgSendTime, nrNodes, nrCollisions, nrReceived, nrProcessed, nrLost, nrTransmissions, OverallEnergy1, OverallEnergy2, der1, der2 = np.loadtxt(result_filepath, unpack=True)
+
+# Create figure directory to save figures
+dirname = "figures"
+path = "./"+dirname
+
+if os.path.isdir(path)==False:
+    os.mkdir(path)
+
+# plot raw data
+plt.figure(1)
+plt.plot(nrNodes, OverallEnergy1, 'r--', nrNodes, OverallEnergy2, 'b')
+plt.xlabel('Nodes')
+plt.ylabel('Overall Energy')
+plt.title('Raw Data')
+plt.savefig(path+"/OverallEnergy_consumption_raw.png")
+#plt.show()
+
+# plot mean data 
+nodes = np.unique(nrNodes)
+m_energy1 = np.mean(np.reshape(OverallEnergy1, [nodes.size,nrRealization]),1)
+m_energy2 = np.mean(np.reshape(OverallEnergy2, [nodes.size,nrRealization]),1)
+m_der1 = np.mean(np.reshape(der1, [nodes.size,nrRealization]),1)
+m_der2 = np.mean(np.reshape(der2, [nodes.size,nrRealization]),1)
+
+plt.figure(2)
+plt.plot(nodes, m_energy1, 'r--', nodes, m_energy2, 'b')
+plt.xlabel('Nodes')
+plt.ylabel('Overall Energy')
+plt.title('Mean Data')
+plt.savefig(path+"/OverallEnergy_consumption_mean.png")
+#plt.show()
+
+plt.figure(3)
+plt.plot(nodes, m_der1, 'r--', nodes, m_der2, 'b*')
+plt.xlabel('Nodes')
+plt.ylabel('Overall Energy')
+plt.title('Mean Data')
+plt.savefig(path+"/data_extraction_rate_mean.png")
+#plt.show()
--- run_sim.sh
+++ run_sim.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # script that lists all the items in the current folder
-nodes=100 			#number of nodes to simulate
-avgsend=1000			#average sending interval in ms
-experiment=0 			#simulation radio settings 0 --> SF12 - BW125 - CR4/8
-simtime=10000			#total running time in ms
-for i in `seq 1 10`;
-do
-	echo =========================================
-	echo Simulation : $i
-	echo -----------------------------------------
-	python loraDir.py $nodes $avgsend $experiment $simtime
+nodes=`seq 100 100 1600`			#number of nodes to simulate
+avgsend=1000000						#average sending interval in ms
+experiment=1 						#simulation radio settings 0 --> SF12 - BW125 - CR4/8
+simdays=7	
+simtime=$((($simdays)*24*60*60*1000))	#total running time in ms
+
+echo =====================================================
+for node in $nodes
+do	
+	for i in `seq 1 100`
+	do
+		echo Simulation : Nodes: $node - Sim: $i --- $((($node)*($i)/(1*1600)))%
+		echo ----------------------------------------------
+		python loraDir.py $node $avgsend $experiment $simtime
+	done
 done
-
-
+echo =====================================================
