19b1fc155e5ea92c9dbc7be9af1c31a608eb9819
KHALIL <fayekhaliloulaye@gmail.com>
2020-07-16 23:54:29 +0000

    modification de simdays et le nombre de realisation

--- run_sim.sh
+++ run_sim.sh
@@ -4,15 +4,15 @@
 nodes=`seq 100 100 1600`			#number of nodes to simulate
 avgsend=1000000						#average sending interval in ms
 experiment=1 						#simulation radio settings 0 --> SF12 - BW125 - CR4/8
-simdays=7	
+simdays=58
 simtime=$((($simdays)*24*60*60*1000))	#total running time in ms
 
 echo =====================================================
 for node in $nodes
 do	
-	for i in `seq 1 100`
+	for i in `seq 1 10`
 	do
-		echo Simulation : Nodes: $node - Sim: $i --- $((($node)*($i)/(1*1600)))%
+		echo Simulation : Nodes: $node - Sim: $i --- $((($node)*($i)/(1*160)))%
 		echo ----------------------------------------------
 		python loraDir.py $node $avgsend $experiment $simtime
 	done
